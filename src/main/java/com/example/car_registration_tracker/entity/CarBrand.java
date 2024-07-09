cdc3747271f54b19bf76df4d6e8b92280c0cc8d1
r3aper89 <56981798+r3aper89@users.noreply.github.com>
2024-07-09 12:32:47 +0000

    fix: added devconteriner extensions and java formatter, created carbrand entity

--- src/main/java/com/example/car_registration_tracker/entity/CarBrand.java
+++ src/main/java/com/example/car_registration_tracker/entity/CarBrand.java
@@ -2,9 +2,18 @@ package com.example.car_registration_tracker.entity;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
+
+@Data
+@Entity
+@Table(name = "car_brands")
 public class CarBrand {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long brandId;
+
+    @Column(nullable = false, unique = true)
+    private String brandName;
     
 }
