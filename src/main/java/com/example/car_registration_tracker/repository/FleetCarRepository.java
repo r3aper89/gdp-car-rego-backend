c7c7bb08b000a87867668f7bf74e4f37fdecfa48
r3aper89 <56981798+r3aper89@users.noreply.github.com>
2024-07-11 07:33:19 +0000

    feat: get from database

--- src/main/java/com/example/car_registration_tracker/repository/FleetCarRepository.java
+++ src/main/java/com/example/car_registration_tracker/repository/FleetCarRepository.java
@@ -1,11 +1,16 @@
 package com.example.car_registration_tracker.repository;
 
-
 import com.example.car_registration_tracker.entity.FleetCar;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface FleetCarRepository extends JpaRepository<FleetCar, Long> {
+    // findBy OrderBy ExpiryDate Asc
     List<FleetCar> findByOrderByExpiryDateAsc();
+    // findBy FleetActive OrderBy ExpiryDate Asc
+    List<FleetCar> findByFleetActiveOrderByExpiryDateAsc(boolean fleetActive);
 }
+// Query Creation
+// findBy: https://docs.spring.io/spring-data/jpa/reference/repositories/query-methods-details.html#repositories.query-methods.query-creation
+// OrderBy: https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
