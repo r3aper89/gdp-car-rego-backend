597a33f3ed0d9ab9cd59354cc3e6071e12c3ed79
r3aper89 <56981798+r3aper89@users.noreply.github.com>
2024-07-11 05:39:03 +0000

    feat: get all car brands

--- src/main/java/com/example/car_registration_tracker/controller/CarBrandController.java
+++ src/main/java/com/example/car_registration_tracker/controller/CarBrandController.java
@@ -1,15 +1,27 @@
 package com.example.car_registration_tracker.controller;
 
 
+import com.example.car_registration_tracker.entity.CarBrand;
+import com.example.car_registration_tracker.service.CarBrandService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/car-brands")
 public class CarBrandController {
     
 
-    // getAllCarBrands()
+    @Autowired
+    private CarBrandService carBrandService;
+
+    @GetMapping
+    public List<CarBrand> getAllCarBrands() {
+        return carBrandService.getAllCarBrands();
+    }
+    
 
     // getCarBrandById()
-    
+
 }
--- src/main/java/com/example/car_registration_tracker/service/CarBrandService.java
+++ src/main/java/com/example/car_registration_tracker/service/CarBrandService.java
@@ -1,10 +1,20 @@
 package com.example.car_registration_tracker.service;
 
-
+import com.example.car_registration_tracker.entity.CarBrand;
+import com.example.car_registration_tracker.repository.CarBrandRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CarBrandService {
 
+    @Autowired
+    private CarBrandRepository carBrandRepository;
+
+    public List<CarBrand> getAllCarBrands() {
+        return carBrandRepository.findAll();
+    }
 
 }
